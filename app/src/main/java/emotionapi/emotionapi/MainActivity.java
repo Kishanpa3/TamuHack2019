66dfd135596a455cbd263fcae4bcd6084876ad63
kishanpa3 <kishanpa@tamu.edu>
2019-01-27 00:00:36 -0600

    [Checkpoint] UI Left

--- app/src/main/java/emotionapi/emotionapi/MainActivity.java
+++ app/src/main/java/emotionapi/emotionapi/MainActivity.java
@@ -1,5 +1,6 @@
 package emotionapi.emotionapi;
 
+import android.app.WallpaperManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -8,35 +9,38 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
-import java.io.Console;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Random;
 
 import cz.msebera.android.httpclient.HttpEntity;
 import cz.msebera.android.httpclient.HttpResponse;
 import cz.msebera.android.httpclient.client.HttpClient;
-import cz.msebera.android.httpclient.entity.ByteArrayEntity;
 import cz.msebera.android.httpclient.client.methods.HttpPost;
-import cz.msebera.android.httpclient.impl.client.HttpClients;
 import cz.msebera.android.httpclient.client.utils.URIBuilder;
+import cz.msebera.android.httpclient.entity.ByteArrayEntity;
+import cz.msebera.android.httpclient.impl.client.HttpClients;
 import cz.msebera.android.httpclient.util.EntityUtils;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,12 +50,36 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CAMERA_CODE = 300;
     private static final int REQUEST_PERMISSION_CODE = 200;
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView mImageView;
+
+    Random rand = new Random();
+    int pageNumber = rand.nextInt(100)+1;
+
+
+    //Google Custom Search API Authentication
+    String cxKey = "011259080533467582698:_oz0hfbrhfe";
+    String apiKey = "AIzaSyAFaaXEHYtqpWIPfz8qvY9DgxDozAaFJFk";
+
+    String color = "white"; //default to neutral color
+    String queryURL = "https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=" + cxKey +
+            "&imgSize=large" +
+            "&imgType=photo" +
+            "&start=" + String.valueOf(pageNumber) +
+            "&q=wallpaper+400x800+" +
+            "&imgDominantColor=" + color +
+            "&searchType=image" +
+            "&fields=url,items(link)";
+
+    HashMap<Integer, String> emotionColorPairs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initEmotionColorMap();
         // initiate our image view and text view
         imageView = (ImageView) findViewById(R.id.imageView);
         resultText = (TextView) findViewById(R.id.resultText);
@@ -97,7 +125,14 @@ public class MainActivity extends AppCompatActivity {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
         }
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
     }
+
 
     // convert image to base 64 so that we can send the image to Emotion API
     public byte[] toBase64(ImageView imgPreview) {
@@ -184,28 +219,148 @@ public class MainActivity extends AppCompatActivity {
         // this function is called when we get a result from the API call
         @Override
         protected void onPostExecute(String result) {
-//            JSONArray jsonArray = null;
+            JSONArray jsonArray = null;
             try {
-                // convert the string to JSONArray
-//                jsonArray = new JSONArray(result);
-                String emotions = "";
-                String jsonString = result.trim();
-                if (jsonString.charAt(0) == '[') {
-                    JSONArray jsonArray = new JSONArray(jsonString);
-                    emotions+=(jsonArray.toString(2));
-                } else if (jsonString.charAt(0) == '{') {
-                    JSONObject jsonObject = new JSONObject(jsonString);
-                    emotions+=(jsonObject.toString(2));
-                } else {
-                    emotions+=(jsonString);
+
+                Double[] emotions = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+
+                JSONArray jArray = new JSONArray(result.trim());
+                for(int i =0; i < jArray.length(); i++)
+                {
+                    JSONObject jObject = jArray.getJSONObject(i).getJSONObject("faceAttributes").getJSONObject("emotion");
+                    emotions[0] += jObject.getDouble("anger");
+                    emotions[1] += jObject.getDouble("contempt");
+                    emotions[2] += jObject.getDouble("disgust");
+                    emotions[3] += jObject.getDouble("fear");
+                    emotions[4] += jObject.getDouble("happiness");
+                    emotions[5] += jObject.getDouble("neutral");
+                    emotions[6] += jObject.getDouble("sadness");
+                    emotions[7] += jObject.getDouble("surprise");
+                }
+                double max =emotions[0]; int maxIndex = 0;
+                for(int i =0; i < 8; i++)
+                {
+                    if(max < emotions[i])
+                    {
+                        max = emotions[i];
+                        maxIndex = i;
+                    }
+                    emotions[i] /= jArray.length();
                 }
 
-                emotions = emotions.substring(emotions.length()/2);
-                resultText.setText(emotions);
+                updateColor(maxIndex);
+                getURLRequest();
 
-            } catch (JSONException e) {
-                resultText.setText("No emotion detected. Try again later");
+                String maxString = String.valueOf(maxIndex);
+                resultText.setText(maxString);
+
+            } catch (Exception e) {
+                resultText.setText(e.getMessage());
             }
         }
+    }
+
+    private void getURLRequest() {
+        //Parses JSON Object and sets Wallpaper
+        WallpaperFactory url = new WallpaperFactory();
+        url.execute();
+    }
+
+//______________________________________________________________________________________________
+//                                  HTTP REQUEST HANDLER
+//_____________________________________________________________________________________________
+
+    private class WallpaperFactory extends AsyncTask <Void, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        //Extracts JSON Object from Image Query URL
+        @Override
+        protected String doInBackground(Void... voids) {
+            JsonReader reader = new JsonReader();
+            String link = null;
+            try {
+                JSONObject object = reader.readJsonFromUrl(queryURL);
+
+                JSONArray urlArray = object.getJSONArray("items");
+//                for (int i = 0; i < urlArray.length(); i++) {
+//                    String link = urlArray.getJSONObject(i).getString("link");
+//                    System.out.println(link);
+//                }
+
+                link = urlArray.getJSONObject(0).getString("link");
+
+                if(link != null){
+                    Bitmap result = Picasso.get().load(link).get();
+                    WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+                    wallpaperManager.setBitmap(result);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return link;
+        }
+
+        @Override
+        protected void onPostExecute(String imageURL) {
+            super.onPostExecute(imageURL);
+
+            if (imageURL == null) {
+                Toast toast = Toast.makeText(MainActivity.this, "Failed To Set Wallpaper", Toast.LENGTH_LONG );
+                toast.show();
+            } else {
+                System.out.println(imageURL);
+                updatePageNumber();
+            }
+        }
+    }
+
+//______________________________________________________________________________________________
+//                                   HELPER FUNCTIONS
+//______________________________________________________________________________________________
+
+    //Reseeds random value for page number
+    private void updatePageNumber() {
+        pageNumber = rand.nextInt(100)+1;
+
+        queryURL = "https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=" + cxKey +
+                "&imgSize=large" +
+                "&imgType=photo" +
+                "&start=" + String.valueOf(pageNumber) +
+                "&q=wallpaper+400x800+" +
+                "&imgDominantColor=" + color +
+                "&searchType=image" +
+                "&fields=url,items(link)";
+    }
+
+
+    void updateColor(int colorKey){
+        String color = emotionColorPairs.get(colorKey);
+
+        queryURL = "https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=" + cxKey +
+                "&imgSize=large" +
+                "&imgType=photo" +
+                "&start=" + String.valueOf(pageNumber) +
+                "&q=wallpaper+400x800+" +
+                "&imgDominantColor=" + color +
+                "&searchType=image" +
+                "&fields=url,items(link)";
+    }
+
+
+    void initEmotionColorMap() {
+        emotionColorPairs = new HashMap<>();
+
+        emotionColorPairs.put(0, "red");
+        emotionColorPairs.put(1, "black");
+        emotionColorPairs.put(2, "green");
+        emotionColorPairs.put(3, "purple");
+        emotionColorPairs.put(4, "yellow");
+        emotionColorPairs.put(5, "white");
+        emotionColorPairs.put(6, "blue");
+        emotionColorPairs.put(7, "orange");
     }
 }
